f5f135df6c678904b8d2667d6144e811408e0ea6
abdulrahmanosoble <abdulrahman.osoble@gmail.com>
2015-09-29 16:51:48 +0400

    Added room.ino
    Added room.ino

---
+++ room.ino
@@ -0,0 +1,131 @@
+
+#include <SoftwareSerial.h>
+#include <Adafruit_CC3000.h>
+#include <ccspi.h>
+#include <SPI.h>
+#include <string.h>
+#include "utility/debug.h"
+
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+// These can be any two pins
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+                                         SPI_CLOCK_DIVIDER); // you can change this clock speed
+
+#define WLAN_SSID       "Lighthouse"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "460Beacon202483"
+// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+#define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
+                                   // received before closing the connection.  If you know the server
+                                   // you're accessing is quick to respond, you can reduce this value.
+
+// What page to grab!
+#define WEBSITE      "www.abdulrahmanosoble.me"
+#define WEBPAGE      "/roomsecurity/"
+
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW and the CC3000 module (called automatically
+            on startup)
+*/
+/**************************************************************************/
+
+uint32_t ip;
+
+int magnet;
+int reed = 4;
+int buzzer = 2;
+
+void sendTweet(){
+  cc3000.printIPdotsRev(ip);
+  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
+ if (www.connected()) {
+    www.fastrprint(F("GET "));
+    www.fastrprint(WEBPAGE);
+    www.fastrprint(F(" HTTP/1.1\r\n"));
+    www.fastrprint(F("Host: ")); www.fastrprint(WEBSITE); www.fastrprint(F("\r\n"));
+    www.fastrprint(F("\r\n"));
+    www.println();
+  } else {
+    Serial.println(F("Connection failed"));    
+    return;
+  }
+
+  Serial.println(F("-------------------------------------"));
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(reed, INPUT);
+  pinMode(buzzer, OUTPUT); 
+  
+  //WIFI PART
+  
+  Serial.begin(115200);
+  Serial.println(F("Hello, CC3000!\n")); 
+
+  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+  
+  /* Initialise the module */
+  Serial.println(F("\nInitializing..."));
+  if (!cc3000.begin())
+  {
+    Serial.println(F("Couldn't begin()! Check your wiring?"));
+    while(1);
+  }
+  
+  // Optional SSID scan
+  // listSSIDResults();
+  
+  Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println(F("Failed!"));
+    while(1);
+  }
+   
+  Serial.println(F("Connected!"));
+  
+  /* Wait for DHCP to complete */
+  Serial.println(F("Request DHCP"));
+  while (!cc3000.checkDHCP())
+  {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  }  
+
+  ip = 0;
+  // Try looking up the website's IP address
+  Serial.print(WEBSITE); Serial.print(F(" -> "));
+  while (ip == 0) {
+    if (! cc3000.getHostByName(WEBSITE, &ip)) {
+      Serial.println(F("Couldn't resolve!"));
+    }
+    delay(500);
+  }
+
+  Serial.println(F("-------------------------------------"));
+}
+
+void loop() {
+  while(digitalRead(reed) == HIGH);
+  while(true)
+  {
+    while(digitalRead(reed) == LOW);
+      delay(700);
+    if(digitalRead(reed) == LOW)
+      break;
+  }
+  for(int i=0;i<5000;i++)
+  {
+    digitalWrite(buzzer, HIGH);
+    delayMicroseconds(200);
+  }
+  sendTweet();
+  digitalWrite(buzzer, LOW);
+}  
